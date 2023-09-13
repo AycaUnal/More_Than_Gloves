b2ebceb25bfbc1decaa81a184678622f73d89076
Ayca S. Unal <108795469+AycaUnal@users.noreply.github.com>
2023-09-13 22:09:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ import pydeck as pdk
 #####################################        STREAMLİT             ##############################################################
 
 #### Sayfa Ayarları #########
-#st.set_page_config(
-    #page_title="More Than Gloves",
-    #page_icon=":gloves:",
-    #layout="wide",
-    #initial_sidebar_state="expanded",
-    #menu_items={
-        #'Get Help': 'https://www.extremelycoolapp.com/help',
-        #'Report a bug': "https://www.extremelycoolapp.com/bug",
-        #'About': "# This is a header. This is an *extremely* cool app!"
-    #}
-#)
+st.set_page_config(
+    page_title="More Than Gloves",
+    page_icon=":gloves:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
 
 
 ###### BAŞLIK #######
